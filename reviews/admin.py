cbe6c13b09f0a3058278e151b89a0274e4cf349c
jaejunkim <aamoof@gmail.com>
2024-08-26 01:57:32 +0900

    #8.4

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,11 +2,51 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [("good", "Good"), ("great", "Great"), ("awesome", "Awesome")]
+
+    def queryset(self, request, reviews):
+        # dir로 뭐가 있는 지 알 수 있음 dir(request)
+        word = self.value()  # url 값
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
+class ReviewFilter(admin.SimpleListFilter):
+    title = "is Review Positive?"
+    parameter_name = "rating"
+
+    def lookups(self, request, model_admin):
+        return [("bad", "Bad"), ("good", "Good")]
+
+    def queryset(self, request, reviews):
+        if self.value() == "bad":
+            return reviews.filter(rating__lt=3)
+        elif self.value() == "good":
+            return reviews.filter(rating__gte=3)
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",  # str로도 컬럼을 사용할 수 있음
         "payload",
+    )
+    # relation_ship 필터도 만들 수 있음
+    # filter는 순차적으로 내려감 WordFilter -> rating -> user__is_host ... -> room__pet_friendly
+    list_filter = (
+        ReviewFilter,
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
     )
 
 
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+# request를 통해 어떤 사람이 요청한지 알 수 있음, 이를 통해 권한이 있는 사람만 요청하게 할 수도 있음
+# queryset은 어떤 요소를 선택한건지 알 수 있음
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
     list_display = (
         "name",
         "price",
@@ -20,6 +30,15 @@ class RoomAdmin(admin.ModelAdmin):
         "kind",
         "amenities",
     )
+    search_fields = (
+        # "name",
+        # "=price",
+        "owner__username",
+    )
+    # 검색활성화, 와일드 카드도 사용가능,
+    # 예) ^name: startwith, 앞에 문자가 같으면, 아무것도 안붙이면 기본값은 포함되었을때임
+    # =는 정확히 같을때
+    # 소유자의 이름으로도 검색 가능, owner__username
 
     # def total_amenities(self, room): # 두번째에 room을 넣는 것은 관리자 패널임
     #     return room.amenities.count()
