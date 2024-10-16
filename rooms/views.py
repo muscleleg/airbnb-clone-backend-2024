702bec6c59e1bf44b248c1419083ac9e8710b40f
Jaejun Kim <jj.kim2@okestro.com>
2024-10-16 13:05:29 +0900

    11.7

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,13 @@
+from unicodedata import category
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.exceptions import NotFound, NotAuthenticated
+from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 
 from .models import Room, Amenity
+from categories.models import Category
 from rest_framework.views import APIView
 
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
@@ -86,7 +89,19 @@ class Rooms(APIView):
             print(dir(request.user))
             serializer = RoomDetailSerializer(data=request.data)
             if serializer.is_valid():
-                room = serializer.save(owner=request.user)
+                category_pk = request.data.get("category")
+                if not category_pk:
+                    raise ParseError
+                try:
+                    category = Category.objects.get(pk=category_pk)
+                    if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                        raise ParseError
+
+
+                except Category.DoesNotExist:
+                    raise ParseError
+
+                room = serializer.save(owner=request.user, category=category)
                 serializer = RoomDetailSerializer(room)
                 return Response(serializer.data)
             else:
