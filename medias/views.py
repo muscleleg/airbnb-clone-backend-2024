777d11fb39d9ec9c1f1016dea214de1219c11d3a
Jaejun Kim <jj.kim2@okestro.com>
2024-10-23 12:56:34 +0900

    11.17 10-10

--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.exceptions import NotFound, PermissionDenied
+from medias.models import Photo
 
-# Create your views here.
+
+class PhotoDetail(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self,pk):
+        try:
+            return Photo.objects.get(pk=pk)
+        except Photo.DoesNotExist:
+            raise NotFound
+
+    def delete(self, request, pk):
+        photo = self.get_object(pk)
+        # if photo.room: # photo는 room과 experience 둘 중 하나에 속할 수 있음
+        #     if photo.room.owner != request.user:
+        #         raise PermissionDenied
+        # elif photo.experience:
+        #     if photo.experience.host != request.user:
+        #         raise PermissionDenied
+
+        if(photo.room and photo.room.owner != request.user) or (photo.experience and photo.experience.host != request.user):
+            raise PermissionDenied
+        photo.delete()
+        return Response(status = HTTP_200_OK)
+
