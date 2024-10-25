13c275340efabd25bf4926d2ab41f2252bf9f14f
Jaejun Kim <jj.kim2@okestro.com>
2024-10-25 12:57:48 +0900

    11.18 -fin

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+from users.serializers import TinyUserSerializer
 from .models import Review
 
 
 class ReviewsSerializer(serializers.ModelSerializer):
+
+    user = TinyUserSerializer(read_only=True)
+
     class Meta:
         model = Review
-        fields = "__all__"
+        fields = ("user", "payload", "rating",)
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.serializers import serialize
 from rest_framework.exceptions import (
     NotFound,
     NotAuthenticated,
@@ -163,6 +164,8 @@ class RoomDetail(APIView):
 
 class RoomReviews(APIView):
 
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
@@ -187,6 +190,17 @@ class RoomReviews(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        serializer = ReviewsSerializer(data=request.data)
+        if serializer.is_valid():
+            review = serializer.save(
+                user=request.user,
+                room=self.get_object(pk)
+            )
+            serializer = ReviewsSerializer(review)
+            return Response(serializer.data)
+
 
 
 class RoomPhotos(APIView):
