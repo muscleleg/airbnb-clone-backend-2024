f5c9136dc2bbc39e630af641d45f2e859f75979a
jaejunkim <aamoof@gmail.com>
2024-10-29 08:56:15 +0900

    11.19 - 9:14

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,4 +1,6 @@
 from rest_framework.views import APIView
+from rest_framework.status import HTTP_200_OK
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Wishlist
@@ -6,13 +8,70 @@ from .serilalizers import WishlistSerializer
 
 
 class Wishlists(APIView):
-
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         all_wishlists = Wishlist.objects.filter(user=request.user)
-        serializer = WishlistSerializer(all_wishlists, many=True)
+        serializer = WishlistSerializer(
+            all_wishlists,
+            many=True,
+            context={"request": request},
+        )
         return Response(serializer.data)
 
     def post(self, request):
-        pass
+        serializer = WishlistSerializer(data=request.data)
+        if serializer.is_valid():
+            wishlist = serializer.save(
+                user=request.user,
+            )
+            serializer = WishlistSerializer(wishlist)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class WishlistDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        wishlist.delete()
+        return Response(status=HTTP_200_OK)
+
+    def put(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            data=request.data,
+            partial=True,
+        )
+
+        if serializer.is_valid():
+            wishlist = serializer.save()
+            serializer = WishlistSerializer(
+                wishlist,
+                context={"request": request},
+            )
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+# self는 인스턴스 메서드에서 인스턴스 자신을 나타내는 파라미터로, 파이썬에서는 메서드 정의 시
+# 첫 번째 파라미터로 self를 반드시 명시해야 합니다. 하지만 메서드 내부에서 다른 메서드를 호출할 때
+# self는 자동으로 전달되기 때문에 직접 파라미터에 넣을 필요는 없습니다.
