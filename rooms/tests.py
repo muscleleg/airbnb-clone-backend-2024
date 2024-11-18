c5bcee378a80fd5500a9cd53a23e9dde19300328
Jaejun Kim <jj.kim2@okestro.com>
2024-11-18 20:04:04 +0900

    #16.0

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestAemenities(APITestCase):
+
+    # test_로 시작하지 않으면 장고는 test코드를 실행하지 않음
+    def test_two_plus_two(self):
+        self.assertEqual(2 + 2, 5, "The math is wrong")
