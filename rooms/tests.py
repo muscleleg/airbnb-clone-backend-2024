3bb4c5df6b196c766bf552a055f2cc17001a6299
Jaejun Kim <jj.kim2@okestro.com>
2024-11-19 21:43:10 +0900

    #16.5

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,6 +1,7 @@
 from rest_framework.test import APITestCase
 from . import models
 from .serializers import AmenitySerializer
+from users.models import User
 
 
 class TestAemenities(APITestCase):
@@ -125,3 +126,28 @@ class TestAmenity(APITestCase):
         response = self.client.delete("/api/v1/rooms/amenities/1")
 
         self.assertEqual(response.status_code, 204)
+
+
+class TestRooms(APITestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="nico",
+        )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+    def test_create_room(self):
+        response = self.client.post("/api/v1/rooms/")
+        self.assertEqual(response.status_code, 403)
+        # 테스트할때는 관리자 계정도 없음, 넣어줘야함
+
+        self.client.force_login(
+            self.user,
+        )
+        # self.client.login(
+        #     username="nico",
+        #     password="123",
+        # )
+        response = self.client.post("/api/v1/rooms/")
+        print(response.json())
